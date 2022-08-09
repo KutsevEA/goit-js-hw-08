fc02ecd3fa5ea639e7e2144ac07cdc19be2d37f0
KutsevEA <kucev.ea@gmail.com>
2022-08-09 13:42:35 +0300

    Let`s play

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.addEventListener('click', showFullScreenPicture);
+
+const markupForGallery = createMarkupForGallery(galleryItems);
+
+function createMarkupForGallery(items) {
+  return items
+    .map(
+      item =>
+        `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`
+    )
+    .join(``);
+}
+
+galleryEl.insertAdjacentHTML('beforeend', markupForGallery);
+
+function showFullScreenPicture(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  new SimpleLightbox('.gallery a');
+}
